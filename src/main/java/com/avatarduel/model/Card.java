227a15fbc158b50e45f584c3ed942eadd9c6a625
lembahdata <lembahdata404@gmail.com>
2020-04-07 20:34:34 +0700

    fix

--- src/main/java/com/avatarduel/model/Card.java
+++ src/main/java/com/avatarduel/model/Card.java
@@ -4,23 +4,60 @@ import com.avatarduel.model.Element;
 import com.avatarduel.model.CardBuilder;
 
 class Card{
+	protected int id;
+	protected String name;
+	protected Element element;
+	protected String description;
+	protected String imagepath;
+	protected boolean isSummoned;
+	protected boolean isSummonable;
+	protected State state;
 
+	public Card(CardBuilder builder){
+		this.id = builder.id;
+		this.name = builder.name;
+		this.element = builder.element;
+		this.description = builder.description;
+		this.imagepath = builder.imagepath;
+		this.isSummoned = false;
+		this.isSummonable = false;
 	}
 
+	public int getId(){
+		return this.id;
+	}
 
+	public Element getElement(){
+		return this.element;
+	}
 
+	public String getName(){
+		return this.name;
+	}
 
+	public String getDescription(){
+		return this.description;
+	}
 
+	public String getPath(){
+		return this.imagepath;
+	}
 
+	public boolean getStatus(){
+		return this.isSummoned;
+	}
 
+	public boolean getSummonable(){
+		return this.isSummonable;
+	}
 
+	public String toString(){
+		String result = "";
+		result += "Id: " + this.id + 
+		"\nName: " + this.name +
+		"\nElement: " + this.element +
+		"\nDescription: " + this.description;
 
-
-
-
-
-
-
-
+		return result;
 	}
 }
